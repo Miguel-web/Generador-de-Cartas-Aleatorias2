aabbfcb47816a600444e29c783da88ba53c9bf73
Miguel-web <miguelgilf@gmail.com>
2023-06-12 21:58:44 +0000

    variable and call and create html elements

--- src/app.js
+++ src/app.js
@@ -9,5 +9,8 @@ window.onload = function() {
   //write your code here
   console.log("Genrador de Cartar");
 };
-
-const htmlCard = document.createElement("div");
+const htmlCard = document.querySelector("#randomcard");
+const randomCardDiv = document.createElement("div");
+randomCardDiv.id = "card";
+randomCardDiv.classList.add("container");
+htmlCard.appendChild(randomCardDiv);
