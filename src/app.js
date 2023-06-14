f444d543c90d4cae33fb27ed3af9adb1240ee6c9
Miguel-web <miguelgilf@gmail.com>
2023-06-14 21:48:36 +0000

    js code insert for of with random function

--- src/app.js
+++ src/app.js
@@ -5,23 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pinta = ["\u2660", "\u2663", "\u2665", "\u2666"];
-let royalty = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+const coreCardBase = () => {
+  let pintas = ["\u2660", "\u2663", "\u2665", "\u2666"];
+  let numeros = ["A", "K", "Q", "J", 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const cards = [];
+  for (let pinta of pintas) {
+    for (let numero of numeros) {
+      cards.push({ pinta, numero });
+    }
+  }
+};
 const htmlFather = document.querySelector("#randomcard");
 
 const card = `<div class="grid-container">
-<div class="card">
-<div>
-<span class="simboltop">simbolo</span>
-</div>
-<div class="number">numero</div>
-<div>
-<span class="simbolbot">simbolo</span>
-</div>
-</div>
-</div>
-`;
+    <div class="card">
+    <div>
+    <span class="pintatop">simbolo</span>
+    </div>
+    <div class="number">numero</div>
+    <div>
+    <span class="pintabot">simbolo</span>
+    </div>
+    </div>
+    </div>
+    `;
 htmlFather.innerHTML += card;
 
 // window.onload = function() {
